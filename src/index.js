ceab4ee407274b5aa1a8ac1ff67048ef722b3bda
Robert Forster <robertforster@Roberts-Mac-mini.local>
2019-08-28 20:42:26 +0100

    map position

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ import style from "./main.css";
 
 var a = new App();
 
+var homebutton;
+var mapbutton;
+var homepage;
+var mappage;
 
 var clickbutton = (evt) => {
-    console.log(evt.target);
+    if(evt.target.id === 'maptabbutton') {
+        homepage.style.display = "none"; 
+        mappage.removeAttribute('style');
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });        
+    } else if(evt.target.id === 'hometabbutton') {
+        mappage.style.display = "none"; 
+        homepage.removeAttribute('style');
+    }
 }
 
 window.addEventListener('load', function() {
 
-    var homebutton = this.document.getElementById('hometabbutton');
-    var mapbutton = this.document.getElementById('maptabbutton');
+    homebutton = this.document.getElementById('hometabbutton');
+    mapbutton = this.document.getElementById('maptabbutton');
+    homepage = this.document.getElementById('homepage');
+    mappage = this.document.getElementById('mappage');
 
     homebutton.addEventListener('click', clickbutton);
     mapbutton.addEventListener('click', clickbutton);
