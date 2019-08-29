b3d22ce44a937d8d7889135ce414f45d233c5fef
Robert Forster <robertforster@Roberts-Mac-mini.local>
2019-08-29 20:31:21 +0100

    branding

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ var a = new App();
 
 var homebutton;
 var mapbutton;
+var profilebutton;
+
 var homepage;
 var mappage;
+var profilepage;
+
+var profilepage;
+var homepagebutton;
+var mappagebutton;
 
 var clickbutton = (evt) => {
     if(evt.target.id === 'maptabbutton') {
         homepage.style.display = "none"; 
+        profilepage.style.display = "none"; 
         mappage.removeAttribute('style');
         map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: -34.397, lng: 150.644 },
@@ -18,7 +26,12 @@ var clickbutton = (evt) => {
         });        
     } else if(evt.target.id === 'hometabbutton') {
         mappage.style.display = "none"; 
+        profilepage.style.display = "none"; 
         homepage.removeAttribute('style');
+    } else if(evt.target.id === 'profiletabbutton') {
+        mappage.style.display = "none"; 
+        homepage.style.display = "none"; 
+        profilepage.removeAttribute('style');
     }
 }
 
@@ -26,10 +39,22 @@ window.addEventListener('load', function() {
 
     homebutton = this.document.getElementById('hometabbutton');
     mapbutton = this.document.getElementById('maptabbutton');
+    profilebutton = this.document.getElementById('profiletabbutton');
+
     homepage = this.document.getElementById('homepage');
     mappage = this.document.getElementById('mappage');
+    profilepage = this.document.getElementById('profilepage');
+
+    homepagebutton = this.document.getElementById('homepagebutton');
+    mappagebutton = this.document.getElementById('mappagebutton');
+    profilepagebutton = this.document.getElementById('profilepagebutton');
 
     homebutton.addEventListener('click', clickbutton);
     mapbutton.addEventListener('click', clickbutton);
+    profilebutton.addEventListener('click', clickbutton);
+
+    hometabbutton.checked = true;
+    mappagebutton.checked = false;
+    profilepagebutton.checked = false;
 
 })
