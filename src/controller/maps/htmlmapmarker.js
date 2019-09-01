d32563bf40cc6ce26debb8fb4b9e77a787914784
Robert Forster <robertforster@Roberts-Mac-mini.local>
2019-09-01 11:54:33 +0100

    tidy code

--- src/controller/maps/htmlmapmarker.js
+++ src/controller/maps/htmlmapmarker.js
@@ -1,26 +1,26 @@
 export class HTMLMapMarker {
-    constructor(latlng, html, map) {      
-      this.latlng = new google.maps.LatLng(latlng.lat, latlng.lng);
-      this.html = html;
-      this.map = map;
+  constructor(latlng, html, map) {
+    this.latlng = new google.maps.LatLng(latlng.lat, latlng.lng);
+    this.html = html;
+    this.map = map;
 
-      this.innerOverlay = new google.maps.OverlayView();
+    this.innerOverlay = new google.maps.OverlayView();
 
-      this.innerOverlay.onAdd = this.onAdd.bind(this);
-      this.innerOverlay.draw = this.draw.bind(this);
-      this.innerOverlay.onRemove = this.onRemove.bind(this);
+    this.innerOverlay.onAdd = this.onAdd.bind(this);
+    this.innerOverlay.draw = this.draw.bind(this);
+    this.innerOverlay.onRemove = this.onRemove.bind(this);
 
-      this.innerOverlay.setMap(map);
+    this.innerOverlay.setMap(map);
 
-    }
-  
-    createDiv() {
-      this.div = document.createElement('div');
-      this.div.style.position = 'absolute';
-      console.log(this.html);
-      if (this.html) {
-        this.div.appendChild(this.html);
-      
+  }
+
+  createDiv() {
+    this.div = document.createElement('div');
+    this.div.style.position = 'absolute';
+    console.log(this.html);
+    if (this.html) {
+      this.div.appendChild(this.html);
+
       google.maps.event.addDomListener(this.div, 'click', event => {
         console.log('you clicked it');
         google.maps.event.trigger(this, 'click');
@@ -30,47 +30,47 @@ export class HTMLMapMarker {
         console.log('you clicked it');
       });
     }
-      
-    }
-  
-    appendDivToOverlay() {
-      const panes = this.innerOverlay.getPanes();
-      console.log(panes);
-      panes.overlayMouseTarget.appendChild(this.div);
-    }
-  
-    positionDiv() {
-      const point = this.innerOverlay.getProjection().fromLatLngToDivPixel(this.latlng);
-      if (point) {
-        this.div.style.left = `${point.x}px`;
-        this.div.style.top = `${point.y}px`;
-      }
-      console.log(point, this.div);
-    }
-  
-    onAdd() {
-      if (!this.div) {
-        this.createDiv();
-        this.appendDivToOverlay();
-      }
-    }
 
-    draw() {      
-      this.positionDiv();
+  }
+
+  appendDivToOverlay() {
+    const panes = this.innerOverlay.getPanes();
+    console.log(panes);
+    panes.overlayMouseTarget.appendChild(this.div);
+  }
+
+  positionDiv() {
+    const point = this.innerOverlay.getProjection().fromLatLngToDivPixel(this.latlng);
+    if (point) {
+      this.div.style.left = `${point.x}px`;
+      this.div.style.top = `${point.y}px`;
     }
-  
-    onRemove() {
-      if (this.div) {
-        this.div.parentNode.removeChild(this.div);
-        this.div = null;
-      }
-    }
-  
-    getPosition() {
-      return this.latlng;
-    }
-  
-    getDraggable() {
-      return false;
+    console.log(point, this.div);
+  }
+
+  onAdd() {
+    if (!this.div) {
+      this.createDiv();
+      this.appendDivToOverlay();
     }
   }
+
+  draw() {
+    this.positionDiv();
+  }
+
+  onRemove() {
+    if (this.div) {
+      this.div.parentNode.removeChild(this.div);
+      this.div = null;
+    }
+  }
+
+  getPosition() {
+    return this.latlng;
+  }
+
+  getDraggable() {
+    return false;
+  }
+}
